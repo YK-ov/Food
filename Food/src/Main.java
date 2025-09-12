54d15a3234b26996a699538ca942112fffa91632
YK-ov <bubkaogo@gmail.com>
2025-09-12 13:49:20 +0200

    3

--- Food/src/Main.java
+++ Food/src/Main.java
@@ -1,8 +1,9 @@
+import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
         Path gasolineFile = Path.of("src/data/nonfood/benzyna.csv");
 
         NonFoodProduct gasoline = NonFoodProduct.fromCsv(gasolineFile);
@@ -26,6 +27,14 @@ public class Main {
         System.out.println(toShow + " new price");
         double toShowAverage = beetRoot.getPrice(2022, 3);
         System.out.println(toShowAverage + " new price average");
+
+        Path foodDirectory = Path.of("src/data/food");
+        Path nonFoodDirectory = Path.of("src/data/nonfood");
+
+        Product.addProducts(FoodProduct::fromCsv, foodDirectory);
+        Product.addProducts(NonFoodProduct::fromCsv, nonFoodDirectory);
+        Product.getProducts().forEach(System.out::println);
+
     }
 
 
--- Food/src/Product.java
+++ Food/src/Product.java
@@ -1,5 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
 public abstract class Product {
     private String name;
+    private static List<Product> products = new ArrayList<>();
 
     protected Product(String name) {
         this.name = name;
@@ -12,6 +24,38 @@ public abstract class Product {
 
     protected void setName(String name) {
         this.name = name;
+    }
+
+    public static void clearProducts() {
+        products.clear();
+    }
+
+    public static void addProducts(Function<Path, Product> functionalObject, Path directoryDataPath) throws FileNotFoundException {  // functional object, interface as parameter to the method here
+        if (directoryDataPath.getFileName().toString().equals("food")) {
+            String[] wholeDirectory = directoryDataPath.toFile().list();
+
+            for (int i = 0; i < Objects.requireNonNull(wholeDirectory).length; i++) {
+                products.add(functionalObject.apply(Path.of(directoryDataPath + "/" + wholeDirectory[i].trim())));
+            }
+
+        }
+        if (directoryDataPath.getFileName().toString().equals("nonfood")) {
+            String[] wholeDirectory = directoryDataPath.toFile().list();
+            for (int i = 0; i < Objects.requireNonNull(wholeDirectory).length; i++) {
+                products.add(functionalObject.apply(Path.of(directoryDataPath + "/" + wholeDirectory[i].trim())));
+            }
+        }
+    }
+
+    public static List<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                '}';
     }
 
     public abstract double getPrice(int year, int month);
